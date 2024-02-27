4e3db82bb887ed166494c865f12d7f258f326ffb
dan <875126243@qq.com>
2024-02-27 18:12:52 +0800

    fix update code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const useToggle = () => {
 function App() {
   const {value, toggle} = useToggle()
   return (
-    <div className="App">
+    <div className="App"> 
       {value && <div>this is div</div>}
       <button onClick={toggle}> toggle </button>
     </div>
